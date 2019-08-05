0fa209186514d4a803fb865ded6d6d2fe46390a3
krtky123 <45131243+krtky123@users.noreply.github.com>
2019-08-05 15:59:34 +0530

    Update Linked_list.cpp

--- LAB_2/Linked_list.cpp
+++ LAB_2/Linked_list.cpp
@@ -1,1 +1,42 @@
+#include <iostream> 
+#define ll long long
+using namespace std;
+
+struct Node  
+{  
+    int y; 
+    int x;  
+    Node *next;  
+}; 
+
+struct Node*head = NULL;
+
+void AddFirst(int x, int y)
+{
+struct Node *new_node=(struct Node *)malloc(sizeof(struct Node));
+new_node->x=x;
+new_node->y=y;
+new_node->next=head;
+head=new_node;
+}
+
+int DelFirst()
+{
+if(head==NULL)
+{
+cout<<"List is Empty";
+return -1;
+}
+Node*temp=head;
+head=head->next;
+free(temp);
+return 0;
+}
+
+int Del(int x, int y)
+{
+}
+
+
+
 
